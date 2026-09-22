664ed0539af48d4aa9b6f3e85d8598bc490d62d4
ewblycom <ewblycom@icloud.com>
2026-09-22 10:58:08 +0200

    Build 7: visible version stamp and cache bust

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
-const CACHE = "doma-v6";
+const CACHE = "doma-v7";
 self.addEventListener("install", () => self.skipWaiting());
 self.addEventListener("activate", (e) => {
   e.waitUntil(caches.keys().then((keys) => Promise.all(keys.map((k) => caches.delete(k)))));
   self.clients.claim();
 });
 self.addEventListener("fetch", (e) => {
-  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
+  e.respondWith(fetch(e.request, { cache: "no-store" }).catch(() => caches.match(e.request)));
 });
