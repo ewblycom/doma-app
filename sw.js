a7debcd7768ed9acdaee48c91663cade09ee58ee
ewblycom <ewblycom@icloud.com>
2026-09-22 13:25:21 +0200

    SW cache v9

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "doma-v8";
+const CACHE = "doma-v9";
 self.addEventListener("install", () => self.skipWaiting());
 self.addEventListener("activate", (e) => {
   e.waitUntil(caches.keys().then((keys) => Promise.all(keys.map((k) => caches.delete(k)))));
