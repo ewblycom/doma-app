dc84f9b8cae40495b9ead75e12002ebeca746dca
ewblycom <ewblycom@icloud.com>
2026-09-22 15:20:19 +0200

    Сброс кэша sw v17

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
-const CACHE = "doma-v9";
-self.addEventListener("install", () => self.skipWaiting());
+const CACHE = "doma-v17";
+self.addEventListener("install", (e) => { self.skipWaiting(); });
 self.addEventListener("activate", (e) => {
   e.waitUntil(caches.keys().then((keys) => Promise.all(keys.map((k) => caches.delete(k)))));
   self.clients.claim();
 });
 self.addEventListener("fetch", (e) => {
-  e.respondWith(fetch(e.request, { cache: "no-store" }).catch(() => caches.match(e.request)));
+  e.respondWith(fetch(e.request, { cache: "reload" }).catch(() => caches.match(e.request)));
 });
