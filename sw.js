e702f9f5dfb5975d2483b76d4ea90cfdb24049c9
ewblycom <ewblycom@icloud.com>
2026-09-22 10:25:03 +0200

    Fix empty home: full data, new dashboard, bust cache v4

--- sw.js
+++ sw.js
@@ -1,17 +1,9 @@
-const CACHE = "doma-v1";
-const ASSETS = ["./","./index.html","./styles.css","./app.js","./data.js","./manifest.json","./icons/icon.svg"];
-self.addEventListener("install", (e) => {
-  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
-  self.skipWaiting();
-});
+const CACHE = "doma-v4";
+self.addEventListener("install", () => { self.skipWaiting(); });
 self.addEventListener("activate", (e) => {
-  e.waitUntil(caches.keys().then((keys) => Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)))));
+  e.waitUntil(caches.keys().then((keys) => Promise.all(keys.map((k) => caches.delete(k)))));
   self.clients.claim();
 });
 self.addEventListener("fetch", (e) => {
-  e.respondWith(caches.match(e.request).then((r) => r || fetch(e.request).then((res) => {
-    const copy = res.clone();
-    caches.open(CACHE).then((c) => c.put(e.request, copy)).catch(() => {});
-    return res;
-  }).catch(() => caches.match("./index.html"))));
+  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
 });
